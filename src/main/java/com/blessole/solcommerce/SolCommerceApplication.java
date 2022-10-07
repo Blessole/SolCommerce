b81eb2c98c866a706337eac4211ef3d381d4e7e4
Sol Kim <101856058+Blessole@users.noreply.github.com>
2022-10-07 14:32:01 +0900

    Initial commit

---
+++ src/main/java/com/blessole/solcommerce/SolCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.blessole.solcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolCommerceApplication.class, args);
+	}
+
+}
